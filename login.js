0e1a45262fe24a4ad1b98011f90f3dad471af56e
alex <alex.johnston@rocketmail.com>
2018-01-17 16:44:26 +0000

    remove login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-function setCookie(){
-    var value = document.getElementById('clientidtext').innerHTML.value;
-    console.log(value);
-    document.cookie = "clientid=" + value;
-    var x = document.cookie;
-    console.log(x);
-};
